55eb5cb9a080d3a2cd1f5659f105c5a950c4517b
José Triana <jjtriana22@gmail.com>
2025-04-24 14:26:31 -0600

    Add files via upload

---
+++ Generador_QR/Generador_QR/Form1.Designer.cs
@@ -0,0 +1,113 @@
+﻿namespace Generador_QR
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnqr = new System.Windows.Forms.Button();
+            this.BtnGuardar = new System.Windows.Forms.Button();
+            this.PicBoxQR = new System.Windows.Forms.PictureBox();
+            this.txtQR = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.saveFileDialog1 = new System.Windows.Forms.SaveFileDialog();
+            ((System.ComponentModel.ISupportInitialize)(this.PicBoxQR)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // btnqr
+            // 
+            this.btnqr.Location = new System.Drawing.Point(38, 92);
+            this.btnqr.Name = "btnqr";
+            this.btnqr.Size = new System.Drawing.Size(58, 50);
+            this.btnqr.TabIndex = 0;
+            this.btnqr.Text = "Generar";
+            this.btnqr.UseVisualStyleBackColor = true;
+            this.btnqr.Click += new System.EventHandler(this.btnqr_Click);
+            // 
+            // BtnGuardar
+            // 
+            this.BtnGuardar.Location = new System.Drawing.Point(173, 92);
+            this.BtnGuardar.Name = "BtnGuardar";
+            this.BtnGuardar.Size = new System.Drawing.Size(58, 50);
+            this.BtnGuardar.TabIndex = 1;
+            this.BtnGuardar.Text = "Guardar";
+            this.BtnGuardar.UseVisualStyleBackColor = true;
+            this.BtnGuardar.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // PicBoxQR
+            // 
+            this.PicBoxQR.Location = new System.Drawing.Point(38, 158);
+            this.PicBoxQR.Name = "PicBoxQR";
+            this.PicBoxQR.Size = new System.Drawing.Size(193, 185);
+            this.PicBoxQR.TabIndex = 2;
+            this.PicBoxQR.TabStop = false;
+            // 
+            // txtQR
+            // 
+            this.txtQR.Location = new System.Drawing.Point(38, 45);
+            this.txtQR.Multiline = true;
+            this.txtQR.Name = "txtQR";
+            this.txtQR.Size = new System.Drawing.Size(193, 32);
+            this.txtQR.TabIndex = 3;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(69, 18);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(132, 13);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "Ingresa el texto a convertir";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(278, 355);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txtQR);
+            this.Controls.Add(this.PicBoxQR);
+            this.Controls.Add(this.BtnGuardar);
+            this.Controls.Add(this.btnqr);
+            this.Name = "Form1";
+            this.Text = "Generador de código QR";
+            ((System.ComponentModel.ISupportInitialize)(this.PicBoxQR)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnqr;
+        private System.Windows.Forms.Button BtnGuardar;
+        private System.Windows.Forms.PictureBox PicBoxQR;
+        private System.Windows.Forms.TextBox txtQR;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.SaveFileDialog saveFileDialog1;
+    }
+}
+
---
+++ Generador_QR/Generador_QR/Form1.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Drawing;
+using System.Drawing.Imaging;
+using System.Windows.Forms;
+
+
+namespace Generador_QR
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnqr_Click(object sender, EventArgs e)
+        {
+            Zen.Barcode.CodeQrBarcodeDraw codigoqr = Zen.Barcode.BarcodeDrawFactory.CodeQr;
+            PicBoxQR.Image = codigoqr.Draw(txtQR.Text, 200);
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            saveFileDialog1.InitialDirectory = Environment.GetFolderPath(Environment.SpecialFolder.Desktop);
+            saveFileDialog1.FileName = "";
+            saveFileDialog1.Filter = "JPEG|*.jpeg";
+
+            if (saveFileDialog1.ShowDialog() != DialogResult.Cancel)
+            {
+                string varimg = saveFileDialog1.FileName;
+                Bitmap varbmp = new Bitmap(PicBoxQR.Image);
+                varbmp.Save(varimg, ImageFormat.Jpeg);
+
+            }
+        }
+    }
+}
